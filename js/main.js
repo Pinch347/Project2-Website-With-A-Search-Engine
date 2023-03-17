c22ba198869cd359c667c0bd8f9b7b50fdae1d99
Pinch347 <128099413+Pinch347@users.noreply.github.com>
2023-03-17 16:59:14 -0500

    Add event listener for form submission

--- js/main.js
+++ js/main.js
@@ -6,4 +6,7 @@ const giphyResults = document.getElementById("giphy-results");
 
 const apiKey = "4G7xDRRWoXU5yqWLtLvwRXvhdiYxEmCE";
 let apiUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&limit=";
- 
+
+searchForm.addEventListener("submit", (event) => {
+    
+  
