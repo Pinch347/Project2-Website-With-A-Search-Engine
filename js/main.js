0862a32606a0d016dee834f7c0cba7ca53d1d185
Pinch347 <128099413+Pinch347@users.noreply.github.com>
2023-03-17 16:58:28 -0500

    Define API key and URL

--- js/main.js
+++ js/main.js
@@ -4,4 +4,6 @@ const searchInput = document.getElementById("search-input");
 const resultsInput = document.getElementById("num-results");
 const giphyResults = document.getElementById("giphy-results");
 
-  
+const apiKey = "4G7xDRRWoXU5yqWLtLvwRXvhdiYxEmCE";
+let apiUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&limit=";
+ 
