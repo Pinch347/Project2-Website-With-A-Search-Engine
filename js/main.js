f933c8c11cd378e1d322dafff01ea5131facca23
Pinch347 <128099413+Pinch347@users.noreply.github.com>
2023-03-17 17:03:29 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const searchForm = document.getElementById("search-form");
-const searchInput = document.getElementById("search-input");
-const resultsInput = document.getElementById("num-results");
-const giphyResults = document.getElementById("giphy-results");
+    const searchInput = document.getElementById("search-input");
+    const resultsInput = document.getElementById("num-results");
+    const giphyResults = document.getElementById("giphy-results");
 
-const apiKey = "4G7xDRRWoXU5yqWLtLvwRXvhdiYxEmCE";
-let apiUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&limit=";
+    const apiKey = "4G7xDRRWoXU5yqWLtLvwRXvhdiYxEmCE";
+    let apiUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&limit=";
 
-searchForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const query = searchInput.value.trim();
-    const limit = parseInt(resultsInput.value);
-    if (query && limit && limit > 0 && limit <= 50) {
-        // code goes here
-      } else {
-        alert("Please enter a valid number of results between 1 and 50.");
-      }
-      
-  
+    searchForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const query = searchInput.value.trim();
+        const limit = parseInt(resultsInput.value);
+        if (query && limit && limit > 0 && limit <= 50) {
+            fetchGifs(query, limit);
+        } else {
+            alert("Please enter a valid number of results between 1 and 50.");
+        }
+    });
